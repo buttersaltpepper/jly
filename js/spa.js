6359d3651c757019a128273c6a5da8bacfcdf656
buttersaltpepper <buttersaltpepper@gmail.com>
2024-09-27 10:02:30 +0930

    add

--- js/spa.js
+++ js/spa.js
@@ -5,7 +5,7 @@ const navItems = document.querySelectorAll('.nav-item a');
 
 const ff = () => {
 	const audio = new Audio("../mooo/hahaaaha.mp3");
-	audio.volume = 0.3;
+	audio.volume = 0.7;
 	return audio;
 };
 
